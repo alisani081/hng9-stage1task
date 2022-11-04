8b2cfa13fd95f71b30114704f8899988f94bb148
alisani081 <960395b7a1e79fe610d98498afeab15d68cbe064>
2022-11-04 18:28:48 +0100

    task 2

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+import re
+import operator
+from enum import Enum
+from typing import Union
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,6 +16,41 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class ComputeOperation(str, Enum):
+    addition = "addition"
+    subtraction = "subtraction"
+    multiplication = "multiplication"
+
+class ComputeData(BaseModel):
+    operation_type: Union[ComputeOperation, str]
+    x: int
+    y: int
+
+
+def getResult(data: ComputeData) -> Union[int, None]:
+    o_type = re.search("add|addition|sub|subtraction|subtract|multiplication|multiply", data.operation_type.lower())
+    if o_type is not None:
+        o_type = o_type.group()
+        ops = {
+            "add": operator.add,
+            "addition": operator.add,
+            "subtraction": operator.sub,
+            "sub": operator.sub,
+            "subtract": operator.sub,
+            "multiplication": operator.mul,
+            "multiply": operator.mul,
+        }
+        if o_type is not None:
+            return ops[o_type](data.x, data.y)
+    return None    
+
+
+@app.post("/compute/")
+def compute_operation(data: ComputeData):
+    if getResult(data):
+        return { "slackUsername": 'ali-sani', "result": getResult(data), "operation_type": data.operation_type  }
+    return HTTPException(422)
 
 
 @app.get("/")
